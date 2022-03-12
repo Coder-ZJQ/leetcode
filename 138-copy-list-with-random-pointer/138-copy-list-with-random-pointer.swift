1805d8f7493c942b450d40b34c0d865ea7538c92
jqz3.tech <zjq_joker@163.com>
2022-03-12 09:23:38 +0800

    Time: 30 ms (56.68%), Space: 15.1 MB (73.27%) - LeetHub

--- 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.swift
+++ 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.swift
@@ -14,29 +14,33 @@
 
 class Solution {
     func copyRandomList(_ head: Node?) -> Node? {
-        let origin = head
-        var head = head
-        while let node = head {
+        var it = head
+        // 第一次遍历：复制下一节点指针
+        // 1,2,3,4
+        // 1,1,2,2,3,3,4,4
+        while let node = it {
             let copy = Node(node.val)
             copy.next = node.next
             node.next = copy
-            head = copy.next
+            it = copy.next
         }
-        head = origin
-        while let node = head {
+        it = head
+        // 第二次遍历：复制随机节点指针
+        while let node = it {
             if let random = node.random {
                 node.next?.random = random.next
             }
-            head = node.next?.next
+            it = node.next?.next
         }
-        head = origin
+        it = head
         let dummy = Node(0)
         var copy: Node? = dummy
-        while let node = head {
+        // 第三次遍历：解开第一次复制的下一节点指针，还原原链表，并链接拷贝的链表
+        while let node = it {
             copy?.next = node.next
             copy = copy?.next
             node.next = node.next?.next
-            head = node.next
+            it = node.next
         }
         return dummy.next
     }
