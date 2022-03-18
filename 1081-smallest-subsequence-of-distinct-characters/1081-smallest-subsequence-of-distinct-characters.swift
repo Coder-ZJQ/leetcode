24b9fe70fe79d34a652122379938b32b0a96489a
jqz3.tech <zjq_joker@163.com>
2022-03-18 10:42:38 +0800

    Time: 9 ms (23.08%), Space: 14.3 MB (61.54%) - LeetHub

---
+++ 1081-smallest-subsequence-of-distinct-characters/1081-smallest-subsequence-of-distinct-characters.swift
@@ -0,0 +1,25 @@
+class Solution {
+    func smallestSubsequence(_ s: String) -> String {
+        var stack = [Character]()
+        var last = [Character: Int](), visited = Set<Character>()
+        // 记录最后的索引
+        for (i, c) in s.enumerated() {
+            last[c] = i
+        }
+        for (i, c) in s.enumerated() {
+            // 如果当前字符已遍历过则跳过该次遍历
+            guard !visited.contains(c) else { continue }
+            // 如果栈顶元素存在，且字典序在当前字符后，且当前字符此后还会出现
+            // 则移除栈顶元素，并更新已遍历集合
+            // 递归一直移除
+            while let top = stack.last, top > c, last[top]! > i {
+                stack.removeLast()
+                visited.remove(top)
+            }
+            // 添加到栈顶，并更新已遍历集合
+            stack.append(c)
+            visited.insert(c)
+        }
+        return String(stack)
+    }
+}
