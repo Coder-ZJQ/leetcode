7cc5b6560f3c8c067589369df7a85e722a53fe31
jqz3.tech <zjq_joker@163.com>
2022-03-22 11:19:01 +0800

    Time: 2010 ms (40.00%), Space: 19.3 MB (60.00%) - LeetHub

--- 1663-smallest-string-with-a-given-numeric-value/1663-smallest-string-with-a-given-numeric-value.swift
+++ 1663-smallest-string-with-a-given-numeric-value/1663-smallest-string-with-a-given-numeric-value.swift
@@ -1,20 +1,20 @@
 class Solution {
     func getSmallestString(_ n: Int, _ k: Int) -> String {
-        var array = [Int](repeating: 1, count: n)
+        var array = [UInt8](repeating: 1, count: n)
         var k = k - n, i = n - 1
         while k > 0 && i >= 0 {
             if k >= 25 {
                 array[i] += 25
                 k -= 25
             } else {
-                array[i] += k
+                array[i] += UInt8(k)
                 break
             }
             i -= 1
         }
-        let chars = [Character]("abcdefghijklmnopqrstuvwxyz")
         return String(array.map {
-            chars[$0 - 1]
+            Character(Unicode.Scalar($0 + 96))
         })
     }
 }
+
