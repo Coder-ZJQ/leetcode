7740097306252361f485aebbd3c009f27e1e2a1a
jqz3.tech <zjq_joker@163.com>
2022-04-07 10:08:54 +0800

    Time: 1123 ms (50.00%), Space: 18.1 MB (100.00%) - LeetHub

--- 1775-equal-sum-arrays-with-minimum-number-of-operations/1775-equal-sum-arrays-with-minimum-number-of-operations.swift
+++ 1775-equal-sum-arrays-with-minimum-number-of-operations/1775-equal-sum-arrays-with-minimum-number-of-operations.swift
@@ -26,19 +26,41 @@ class Solution {
         // 54321
         var res = 0, i = 1, j = 6
         while sum1 < sum2 {
-            if i + j > 6 {
-                if counts2[j] > 0 {
-                    sum2 -= j - 1
-                    res += 1
-                    counts2[j] -= 1
+            if i + j > 7 {
+                let count = counts2[j]
+                if count > 0 {
+                    let reduce = (j - 1) * count
+                    if sum1 < sum2 - reduce {
+                        sum2 -= reduce
+                        res += count
+                        counts2[j] = 0
+                        j -= 1
+                    } else {
+                        let remain = (sum2 - sum1) / (j - 1)
+                        let op = (sum2 - sum1) % (j - 1) == 0 ? remain : remain + 1
+                        sum2 -= op * (j - 1)
+                        res += op
+                        counts2[j] -= op
+                    }
                 } else {
                     j -= 1
                 }
             } else {
-                if counts1[i] > 0 {
-                    sum1 += 6 - i
-                    res += 1
-                    counts1[i] -= 1
+                let count = counts1[i]
+                if count > 0 {
+                    let increase = (6 - i) * count
+                    if sum1 + increase < sum2 {
+                        sum1 += increase
+                        res += count
+                        counts1[i] = 0
+                        i += 1
+                    } else {
+                        let remain = (sum2 - sum1) / (6 - i)
+                        let op = (sum2 - sum1) % (6 - i) == 0 ? remain : remain + 1
+                        sum1 += op * (6 - i)
+                        res += op
+                        counts1[i] -= op
+                    }
                 } else {
                     i += 1
                 }
