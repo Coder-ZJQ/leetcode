6bdc5d3c5b4315065f63b84ecd91b2f2c4d74cd7
jqz3.tech <zjq_joker@163.com>
2022-04-19 00:32:34 +0800

    Time: 8 ms (73.53%), Space: 14.3 MB (70.59%) - LeetHub

--- 821-shortest-distance-to-a-character/821-shortest-distance-to-a-character.swift
+++ 821-shortest-distance-to-a-character/821-shortest-distance-to-a-character.swift
@@ -1,30 +1,25 @@
 class Solution {
     func shortestToChar(_ s: String, _ c: Character) -> [Int] {
         let chars = [Character](s)
-        // right to left
-        var l2r = [Int](repeating: chars.count, count: chars.count)
-        // left to right
-        var r2l = [Int](repeating: chars.count, count: chars.count)
+        var res = [Int](repeating: chars.count, count: chars.count)
         
         var index = 0
+        // left to right
         let start = chars.firstIndex(of: c)!
         for i in start..<chars.count {
             if chars[i] == c {
                 index = i
             }
-            l2r[i] = i - index
+            res[i] = i - index
         }
-        
+        // right to left
         let end = chars.lastIndex(of: c)!
         for i in (0...end).reversed() {
             if chars[i] == c {
                 index = i
             }
-            r2l[i] = index - i
+            res[i] = min(res[i], index - i)
         }
-        
-        return zip(l2r, r2l).map {
-            min($0, $1)
-        }
+        return res
     }
 }
