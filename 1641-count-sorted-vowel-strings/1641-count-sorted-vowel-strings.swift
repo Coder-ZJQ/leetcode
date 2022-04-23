3677f2ee810a2a6771dc0af8d1b2151d80e810db
jqz3.tech <zjq_joker@163.com>
2022-04-23 14:01:58 +0800

    Time: 3 ms (80.00%), Space: 13.4 MB (100.00%) - LeetHub

---
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.swift
@@ -0,0 +1,16 @@
+class Solution {
+    func countVowelStrings(_ n: Int) -> Int {
+        var a = 1, e = 1, i = 1, o = 1, u = 1
+        var k = 1
+        while k < n {
+            a += e + i + o + u
+            e += i + o + u
+            i += o + u
+            o += u
+//            u = u
+            k += 1
+        }
+        
+        return a + e + i + o + u
+    }
+}
