d637249b1e26ea165d78e76c95d7e8a1b0c4d9a4
jqz3.tech <zjq_joker@163.com>
2022-05-04 21:05:05 +0800

    Time: 9 ms (31.25%), Space: 13.5 MB (81.25%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.swift
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.swift
@@ -1,8 +1,8 @@
 class Solution {
     func findTheWinner(_ n: Int, _ k: Int) -> Int {
-        guard n > 1 else { return 1 }
-        var res = 1
-        for i in 2...n {
+        var res = 0
+        // f(n, k) = (f(n - 1, k) + k) % n
+        for i in 1...n {
             res = (k + res - 1) % i + 1
         }
         return res
