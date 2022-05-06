4823d97a5ca3ff40fa7d016a48c887c0b1c15607
jqz3.tech <zjq_joker@163.com>
2022-05-06 10:16:35 +0800

    Time: 102 ms (50.00%), Space: 22.4 MB (6.90%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.swift
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.swift
@@ -1,30 +1,27 @@
 class Node {
+    /// 当前字符
     var char: Character
+    /// 当前字符连续重复数量
     var count: Int
+    
     init(_ char: Character, _ count: Int) {
         self.char = char
         self.count = count
     }
 }
 
-
 class Solution {
     func removeDuplicates(_ s: String, _ k: Int) -> String {
-        // 字符串不能为空，连续重复字符需超过 1
-        guard s.count > 0 && k > 1 else { return "" }
         let chars = [Character](s)
         var stack = [Node]()
         for c in chars {
-            if let last = stack.last {
-                if last.char == c {
-                    if last.count == k - 1 {
-                        let n = stack.count
-                        stack.removeSubrange((n - k + 1)...(n - 1))
-                    } else {
-                        stack.append(Node(c, last.count + 1))
-                    }
+            if let last = stack.last, last.char == c {
+                // 达到数量移除
+                if last.count == k - 1 {
+                    let n = stack.count
+                    stack.removeSubrange((n - k + 1)...(n - 1))
                 } else {
-                    stack.append(Node(c, 1))
+                    stack.append(Node(c, last.count + 1))
                 }
             } else {
                 stack.append(Node(c, 1))
