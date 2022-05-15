69c6a475b6390ceef805a3400647a70ada274ea5
jqz3.tech <zjq_joker@163.com>
2022-05-15 09:28:06 +0800

    Time: 25 ms (33.33%), Space: 14.3 MB (100.00%) - LeetHub

--- 812-largest-triangle-area/812-largest-triangle-area.swift
+++ 812-largest-triangle-area/812-largest-triangle-area.swift
@@ -1,13 +1,26 @@
 class Solution {
     func largestTriangleArea(_ points: [[Int]]) -> Double {
-        var area = 0.0
-        for i in points {
-            for j in points {
-                for k in points {
-                    area = max(area, 0.5 * Double(abs(i[0] * j[1] + j[0] * k[1] + k[0] * i[1] - j[0] * i[1] - k[0] * j[1] - i[0] * k[1])))
+        var res: Double = 0.0
+        for point1 in points {
+            for point2 in points {
+                for point3 in points {
+                    let x = point1[0]
+                    let y = point1[1]
+                    
+                    // 向量一
+                    let v1x = point2[0] - x
+                    let v1y = point2[1] - y
+                    
+                    // 向量二
+                    let v2x = point3[0] - x
+                    let v2y = point3[1] - y
+                    
+                    // 向量叉乘
+                    let crossProduct = Double(abs(v1x * v2y - v2x * v1y)) / 2.0
+                    res = max(res, crossProduct)
                 }
             }
         }
-        return area
+        return res
     }
 }
