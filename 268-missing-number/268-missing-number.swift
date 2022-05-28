3cb08a0ef6e7095aeebc2e43821e2138209624e1
jqz3.tech <zjq_joker@163.com>
2022-05-28 09:23:36 +0800

    Time: 124 ms (66.53%), Space: 14.5 MB (29.77%) - LeetHub

--- 268-missing-number/268-missing-number.swift
+++ 268-missing-number/268-missing-number.swift
@@ -1,6 +1,5 @@
 class Solution {
     func missingNumber(_ nums: [Int]) -> Int {
-        // 异或操作
-        nums.reduce(0, ^) ^ (0...nums.count).reduce(0, ^)
+        (0...nums.count).reduce(0, +) - nums.reduce(0, +)
     }
 }
