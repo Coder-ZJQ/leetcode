4ad6027072c4b94b39dae27365ee85779ee12f25
jqz3.tech <zjq_joker@163.com>
2022-06-03 09:41:24 +0800

    Time: 3185 ms (56.52%), Space: 17.1 MB (93.48%) - LeetHub

--- 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.swift
+++ 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.swift
@@ -1,33 +1,36 @@
 
 class NumMatrix {
-
-    private var sumMatrix: [[Int]]
     
+    private var sums: [[Int]]
+
     init(_ matrix: [[Int]]) {
-        sumMatrix = matrix
         let m = matrix.count, n = matrix[0].count
-        for r in 0..<m {
-            for c in 1..<n {
-                sumMatrix[r][c] += sumMatrix[r][c - 1]
+        sums = matrix
+        for i in 1..<m {
+            sums[i][0] += sums[i - 1][0]
+        }
+        for i in 1..<n {
+            sums[0][i] += sums[0][i - 1]
+        }
+        
+        for i in 1..<m {
+            for j in 1..<n {
+                sums[i][j] += sums[i - 1][j] + sums[i][j - 1] - sums[i - 1][j - 1]
             }
         }
-        for r in 1..<m {
-            for c in 0..<n {
-                sumMatrix[r][c] += sumMatrix[r - 1][c]
-            }
-        }
+        
     }
     
     func sumRegion(_ row1: Int, _ col1: Int, _ row2: Int, _ col2: Int) -> Int {
-        var sum = sumMatrix[row2][col2]
+        var sum = sums[row2][col2]
         if row1 > 0 {
-            sum -= sumMatrix[row1 - 1][col2]
+            sum -= sums[row1 - 1][col2]
         }
         if col1 > 0 {
-            sum -= sumMatrix[row2][col1 - 1]
+            sum -= sums[row2][col1 - 1]
         }
         if row1 > 0 && col1 > 0 {
-            sum += sumMatrix[row1 - 1][col1 - 1]
+            sum += sums[row1 - 1][col1 - 1]
         }
         return sum
     }
