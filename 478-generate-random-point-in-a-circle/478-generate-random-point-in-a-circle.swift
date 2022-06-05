ec7e6c1ca7107a103f3688cbd2045165cc70d3e4
jqz3.tech <zjq_joker@163.com>
2022-06-05 09:11:38 +0800

    Time: 371 ms (100.00%), Space: 19.8 MB (100.00%) - LeetHub

---
+++ 478-generate-random-point-in-a-circle/478-generate-random-point-in-a-circle.swift
@@ -0,0 +1,28 @@
+class Solution {
+    
+    private var radius: Double
+    private var x_center: Double
+    private var y_center: Double
+
+    init(_ radius: Double, _ x_center: Double, _ y_center: Double) {
+        self.radius = radius
+        self.x_center = x_center
+        self.y_center = y_center
+    }
+    
+    func randPoint() -> [Double] {
+        while true {
+            let x_random = Double.random(in: -radius...radius)
+            let y_random = Double.random(in: -radius...radius)
+            if x_random * x_random + y_random * y_random <= radius * radius {
+                return [x_center + x_random, y_center + y_random]
+            }
+        }
+    }
+}
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * let obj = Solution(radius, x_center, y_center)
+ * let ret_1: [Double] = obj.randPoint()
+ */
