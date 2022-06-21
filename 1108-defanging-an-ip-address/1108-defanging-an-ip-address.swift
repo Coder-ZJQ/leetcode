06e90c74178abb425c9af1d48b96f0c3c2c7be05
jqz3.tech <zjq_joker@163.com>
2022-06-22 00:15:00 +0800

    Time: 0 ms (100.00%), Space: 14.3 MB (75.26%) - LeetHub

--- 1108-defanging-an-ip-address/1108-defanging-an-ip-address.swift
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address.swift
@@ -1,13 +1,5 @@
 class Solution {
     func defangIPaddr(_ address: String) -> String {
-        var res = ""
-        for char in address {
-            if char == "." {
-                res += "[.]"
-            } else {
-                res.append(char)
-            }
-        }
-        return res
+        address.split(separator: ".").joined(separator: "[.]")
     }
 }
