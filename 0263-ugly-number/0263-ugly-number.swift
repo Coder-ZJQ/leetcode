a9434879b51ea89da4a1d5c152c3a4a16691a93f
jqz3.tech <zjq_joker@163.com>
2022-11-18 15:56:47 +0800

    Time: 5 ms (66.67%), Space: 13.8 MB (24.56%) - LeetHub

---
+++ 0263-ugly-number/0263-ugly-number.swift
@@ -0,0 +1,16 @@
+class Solution {
+    func isUgly(_ n: Int) -> Bool {
+        guard n != 0 else { return false }
+        var n = n
+        while n % 2 == 0 {
+            n /= 2
+        }
+        while n % 3 == 0 {
+            n /= 3
+        }
+        while n % 5 == 0 {
+            n /= 5
+        }
+        return n == 1
+    }
+}
