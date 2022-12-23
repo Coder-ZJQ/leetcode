f1b31944f4b4ae41f915bfb150e799f3b5509676
jqz3.tech <zjq_joker@163.com>
2022-12-23 10:52:35 +0800

    Time: 89 ms (20.33%), Space: 14.5 MB (64.23%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.swift
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.swift
@@ -2,10 +2,13 @@ class Solution {
     func maxProfit(_ prices: [Int]) -> Int {
         let n = prices.count
         guard n > 1 else { return 0 }
-        var res = 0
+        var res = 0, pre = prices.first!
         // 累加所有上升
-        for i in 1..<n where prices[i] > prices[i - 1] {
-            res += (prices[i] - prices[i - 1])
+        for price in prices[1...] {
+            if price > pre {
+                res += price - pre   
+            }
+            pre = price
         }
         return res
     }
