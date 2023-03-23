31737f490f6d8da7f6b5b581d9123b9e857a0fbd
jqz3.tech <zjq_joker@163.com>
2023-03-23 10:57:42 +0800

    Time: 109 ms (100.00%), Space: 14.2 MB (93.88%) - LeetHub

--- 1630-arithmetic-subarrays/1630-arithmetic-subarrays.swift
+++ 1630-arithmetic-subarrays/1630-arithmetic-subarrays.swift
@@ -1,26 +1,32 @@
 class Solution {
     func checkArithmeticSubarrays(_ nums: [Int], _ l: [Int], _ r: [Int]) -> [Bool] {
         zip(l, r).map { left, right in
-            let subarray = nums[left...right]
-            let max = subarray.max()!
-            let min = subarray.min()!
-            guard (max - min) % (subarray.count - 1) == 0 else {
+            let subNums = nums[left...right]
+            let maxNum = subNums.max()!
+            let minNum = subNums.min()!
+            let n = right - left
+            // 间隔为整数
+            guard (maxNum - minNum) % n == 0 else {
                 return false
             }
-            let delta = (max - min) / (subarray.count - 1)
-            guard delta > 0 else {
+            let gap = (maxNum - minNum) / n
+            if gap == 0 {
                 return true
             }
-            var bools = [Bool](repeating: false, count: subarray.count)
-            for num in subarray {
-                let diff = num - min
-                guard diff % delta == 0 else {
+            /// 用来纪录是否已经枚举过
+            var bools = [Bool](repeating: false, count: n + 1)
+            for num in subNums {
+                // 相差必须能被间隔整除
+                guard (num - minNum) % gap == 0 else {
                     return false
                 }
-                let index = diff / delta
-                guard index < subarray.count, !bools[index] else {
+                // 计算索引
+                let index = (num - minNum) / gap
+                // 已经枚举过即重复了则直接返回 false
+                if bools[index] {
                     return false
                 }
+                // 纪录
                 bools[index] = true
             }
             return true
