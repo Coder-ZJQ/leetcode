8c94c696ab0f2489d3f5b27a1ca032067fc9efed
jqz3.tech <zjq_joker@163.com>
2023-04-09 10:55:11 +0800

    Time: 15 ms (20.00%), Space: 14.9 MB (40.00%) - LeetHub

--- 2399-check-distances-between-same-letters/2399-check-distances-between-same-letters.swift
+++ 2399-check-distances-between-same-letters/2399-check-distances-between-same-letters.swift
@@ -1,13 +1,15 @@
 class Solution {
     func checkDistances(_ s: String, _ distance: [Int]) -> Bool {
-        var indexes = [Character: Int]()
+        var indexes = [Int](repeating: -1, count: 26)
         for (i, char) in s.enumerated() {
-            if let pre = indexes[char] {
-                if i - pre - 1 != distance[Int(char.asciiValue!) - 97] {
+            let char2index = Int(char.asciiValue!) - 97
+            let preIndex = indexes[char2index]
+            if preIndex >= 0 {
+                if i - preIndex - 1 != distance[char2index] {
                     return false
                 }
             } else {
-                indexes[char] = i
+                indexes[char2index] = i
             }
         }
         return true
