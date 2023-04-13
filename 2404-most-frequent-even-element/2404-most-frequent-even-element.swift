dac478203392bcca09777cd66e2f0b09ba97de73
jqz3.tech <zjq_joker@163.com>
2023-04-13 09:14:22 +0800

    Time: 287 ms (15.38%), Space: 14.5 MB (15.38%) - LeetHub

---
+++ 2404-most-frequent-even-element/2404-most-frequent-even-element.swift
@@ -0,0 +1,14 @@
+class Solution {
+    func mostFrequentEven(_ nums: [Int]) -> Int {
+        var freq = [Int: Int]()
+        for num in nums where num % 2 == 0 {
+            freq[num] = freq[num, default: 0] + 1
+        }
+        return freq.sorted {
+            if $0.value == $1.value {
+                return $0.key > $1.key
+            }
+            return $0.value < $1.value
+        }.last?.key ?? -1
+    }
+}
