91ad2ad4a19f25882177d34b273425bed2be47aa
jqz3.tech <zjq_joker@163.com>
2023-04-13 09:15:52 +0800

    Time: 276 ms (46.15%), Space: 14.2 MB (69.23%) - LeetHub

--- 2404-most-frequent-even-element/2404-most-frequent-even-element.swift
+++ 2404-most-frequent-even-element/2404-most-frequent-even-element.swift
@@ -4,11 +4,11 @@ class Solution {
         for num in nums where num % 2 == 0 {
             freq[num] = freq[num, default: 0] + 1
         }
-        return freq.sorted {
+        return freq.max {
             if $0.value == $1.value {
                 return $0.key > $1.key
             }
             return $0.value < $1.value
-        }.last?.key ?? -1
+        }?.key ?? -1
     }
 }
