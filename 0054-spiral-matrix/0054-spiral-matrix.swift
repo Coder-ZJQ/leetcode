d1c6f116deddde179514f3aef2f025f0dc2602d2
jqz3.tech <zjq_joker@163.com>
2023-05-09 22:18:37 +0800

    Time: 0 ms (100.00%), Space: 13.8 MB (94.36%) - LeetHub

---
+++ 0054-spiral-matrix/0054-spiral-matrix.swift
@@ -0,0 +1,43 @@
+class Solution {
+    func spiralOrder(_ matrix: [[Int]]) -> [Int] {
+        let n = matrix.count, m = matrix[0].count
+        var top = 0, bottom = n - 1
+        var left = 0, right = m - 1
+        var res = [Int]()
+        while true {
+            // >
+            for i in left...right {
+                res.append(matrix[top][i])
+            }
+            top += 1
+            if left > right || top > bottom { break }
+            
+            // ∨
+            for i in top...bottom {
+                res.append(matrix[i][right])
+            }
+            right -= 1
+            if left > right || top > bottom { break }
+            
+            // <
+            var i = right
+            while i >= left {
+                res.append(matrix[bottom][i])
+                i -= 1
+            }
+            bottom -= 1
+            if left > right || top > bottom { break }
+            
+            // ^
+            i = bottom
+            while i >= top {
+                res.append(matrix[i][left])
+                i -= 1
+            }
+            left += 1
+            if left > right || top > bottom { break }
+            
+        }
+        return res
+    }
+}
