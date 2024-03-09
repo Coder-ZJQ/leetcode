bc32a3f7252a578cd8eb6dab0cf8948d645f1dca
jqz3.tech <zjq_joker@163.com>
2024-03-09 10:48:10 +0800

    Time: 298 ms (26.09%), Space: 20.4 MB (73.91%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.swift
+++ 2540-minimum-common-value/2540-minimum-common-value.swift
@@ -1,5 +1,34 @@
 class Solution {
-    func getCommon(_ nums1: [Int], _ nums2: [Int]) -> Int {
+    
+    /// 双指针
+    private func getCommonWithTwoPointer(_ nums1: [Int], _ nums2: [Int]) -> Int {
+        var i = 0, j = 0
+        let m = nums1.count, n = nums2.count
+        while i < m {
+            let num1 = nums1[i]
+            var temp = j
+            while temp < n {
+                let num2 = nums2[temp]
+                if num1 == num2 {
+                    return num1
+                } else if num1 < num2 {
+                    j = temp
+                    break
+                }
+                temp += 1
+            }
+            i += 1
+        }
+        return -1
+    }
+    
+    /// 集合
+    private func getCommonWithSet(_ nums1: [Int], _ nums2: [Int]) -> Int {
         Set(nums1).intersection(Set(nums2)).min() ?? -1
+    }
+    
+    func getCommon(_ nums1: [Int], _ nums2: [Int]) -> Int {
+//        getCommonWithSet(nums1, nums2)
+        getCommonWithTwoPointer(nums1, nums2)
     }
 }
